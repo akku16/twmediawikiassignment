0f7f22e1fd03bb151a1f8dfa406a2eb9e4039a37
Akshar <aksharsri@gmail.com>
2021-12-14 17:36:59 +0530

    Create NIC and NSG

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,3 +35,36 @@ resource "azurerm_public_ip" "mediawiki-public-ip" {
     resource_group_name     = azurerm_resource_group.mediawikiResGrp.name
     allocation_method       = "Dynamic"    
 }
+
+# NSG Creation
+resource "azurerm_network_security_group" "mediawiki-nsg" {
+    name            = "mediawikiNSG"
+    location        = "eastus"
+    resource_group_name     = azurerm_resource_group.mediawikiResGrp.name
+    
+    security_rule {
+        name                       = "SSH"
+        priority                   = 1001
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "22"
+        source_address_prefix      = "*"
+        destination_address_prefix = "*" 
+    }
+}
+
+# NIC creation
+resource "azurerm_network_interface" "mediawiki-nic" {
+    name                    = "mediawikiNIC"
+    location                = "eastus"
+    resource_group_name     = azurerm_resource_group.mediawikiResGrp.name
+    
+    ip_configuration {
+        name                            = "mediawikiNICConfig"
+        subnet_id                       = azurerm_subnet.mediawikiSubnet.name
+        private_ip_address_allocation   = "Dynamic"
+        public_ip_address_id            = azurerm_public_ip.mediawikiPublicIP.name
+    }
+}
